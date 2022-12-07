1d06b7c272283fa45fb70ff783afa123f276ae0e
Reed Mitchell <102206070+consumeReed@users.noreply.github.com>
2022-12-07 14:34:44 -0500

    Showdown changes and name
    Changed collection name in pokeapi and added japanese name. Added comments to showdown code and get more get methods

--- RetrieveShowdown.py
+++ RetrieveShowdown.py
@@ -1,31 +1,37 @@
 import pymongo
 import requests
 
+#Establish connection to Mongo DB
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
 mydb = myclient["pokedex"]
 showcol = mydb["showdown"]
 
-
+#Returns string of pokemons name based on its dex no
 def getDex(num):
     pkquery = {"num": num}
     pkname = showcol.find(pkquery, {'name': 1, "_id": 0})
     return pkname[0]['name']
 
-test = getDex(3)
-print(test)
-
+#Returns list of strings of the pokemon's type(s)
 def getTypes(name):
     pkquery = { "name": name}
     pktypes = showcol.find(pkquery, {"types": 1, "_id": 0})
     return pktypes[0]['types']
 
-test2 = getTypes('bulbasaur')
-print(test2)
-
+#Returns a list of strings of the pokemon's stats
 def getStats(name):
     pkquery = { "name": name}
     pkstats = showcol.find(pkquery, {"baseStats": 1, "_id": 0})
     return pkstats[0]['baseStats']
 
-test3 = getStats('bulbasaur')
-print(test3)
+#Returns weight value of pokemon
+def getWeight(name):
+    pkquery = { "name": name}
+    pkstats = showcol.find(pkquery, {"weight": 1, "_id": 0})
+    return pkstats[0]['weight']
+
+#Returns height value of pokemon
+def getHeight(name):
+    pkquery = { "name": name}
+    pkstats = showcol.find(pkquery, {"weight": 1, "_id": 0})
+    return pkstats[0]['height']
--- Showdown.py
+++ Showdown.py
@@ -1,8 +1,11 @@
 import requests
 import pymongo
 
+#Setting up the connection to Mongo DB
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
 mydb = myclient["pokedex"]
+
+#Dropping columns if they exist, and creating a new one
 mycol = mydb["ALLPKMN"]
 mycol.drop()
 mycol = mydb["ALLPKMN"]
@@ -11,20 +14,24 @@ showcol = mydb["showdown"]
 showcol.drop()
 showcol = mydb["showdown"]
 
+#Pokeapi url that has 151 pokemon 
 allurl = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
 
 allresponse = requests.get(allurl)
 alldata = allresponse.json()
 
+#Storing each pokemon and its dex number in Mongo DB
 for i in range(151):
     tmp = { "name": alldata['results'][i]['name'], "dexno": i+1}
     mycol.insert_one(tmp)
 
+#Pokemon Showdown url that will be used to get more detailed info about pokemon
 showdownurl = 'https://play.pokemonshowdown.com/data/pokedex.json'
 
 showdownresponse = requests.get(showdownurl)
 showdowndata = showdownresponse.json()
 
+#Storing more data about each pokemon
 for j in range(151):
     pkquery = { "dexno": j+1}
     pkname = mycol.find(pkquery, {"name": 1, "_id": 0})
