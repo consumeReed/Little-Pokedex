149bcbb9c5cd29427012ca837c06a67fc7b9bffc
kpeaks99 <kpeaks99@gmail.com>
2022-12-12 17:34:48 -0500

    Stores link instead of downloading cry

--- megaCries.py
+++ megaCries.py
@@ -11,32 +11,21 @@ mydb = myclient["pokedex"]
 mega = Mega()
 m = mega.login("peaksk85@students.rowan.edu", "Kingdom#7467sora")
 
-#cries dir path
-path = "./pokecries"
-dir = os.listdir(path)
-
 #Dropping columns if they exist, and creating a new one
 mycol = mydb["cries"]
+mycol.drop()
+mycol = mydb["cries"]
 
-####-------- WinError 32 ------######
-#### https://stackoverflow.com/questions/68465210/problem-downloading-mega-files-with-python/68465437#68465437
-#### link tells you how to fix it ####
-# SPECIFY DOWNLOAD LOCATION
-if len(dir) == 0:
-    mycol.drop()
-    mycol = mydb["cries"]
-    i = 1
-    for i in range(1, 3):
-        filename = str(i)+".mp3"
-        file = m.find(filename)
-        m.download(file, './pokeCries')
-        path = os.getcwd()+"\pokeCries\/"+str(i)+".mp3"
-        tmp = { "dexno": i, "path": path}
-        mycol.insert_one(tmp)
-        i+=1
-    print("done")
-else:
-    print("not empty")
+# Grabs link and puts it into cries db
+print("Starting link retrieval from api. This may take a few minutes")
+for i in range(1, 152):
+    filename = str(i)+".mp3"
+    file = m.find(filename)
+    link = m.get_link(file)
+    print(str(i) + " of 151")
+    tmp = { "dexno": i, "url": link}
+    mycol.insert_one(tmp)
+
 
 #def saveCry(pokeDex):
 #    filename = pokeDex
@@ -45,6 +34,6 @@ else:
 
 def getCry(num):
     pkquery = {"dexno": num}
-    pkname = mycol.find(pkquery, {'path': 1, "_id": 0})
-    return pkname[0]['path']
+    pkname = mycol.find(pkquery, {'url': 1, "_id": 0})
+    return pkname[0]['url']
 
